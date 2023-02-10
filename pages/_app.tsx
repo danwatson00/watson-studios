aae48b9c4b24522567da3e261b1559bd55f01739
Dan Watson <danwatson00@yahoo.com>
2023-02-10 09:21:15 -0600

    next js and mui added and working without flicker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../utils/theme";
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppProps {
+  Component: any,
+  emotionCache: any,
+  pageProps: any
 }
+
+function MyApp ({ Component, emotionCache = clientSideEmotionCache, pageProps }: AppProps){
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
